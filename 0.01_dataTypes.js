a89aaaf4a25f539df47f32cf2ab8ce1d8b6ef9f0
Jared Burris <jaredburris97@gmail.com>
2017-08-16 13:16:23 -0400

    Added dataTypes.js

---
+++ 0.01_dataTypes.js
@@ -0,0 +1,70 @@
+//DataTypes
+
+//Strings
+//Strings are a collection of characters
+
+
+"What a Wonderful World";
+'Hello you guys';
+
+"it's a wonderful day";
+
+console.log('Thomas said "I think grass is green!"'.toUpperCase());
+
+console.log('Couldn\'t things get any better?'.length);
+
+//Challenge create a string that has your name and use the to lowercase function
+// to make sure each character is lower case
+
+console.log('Jared Christopher Burris'.toLowerCase());
+
+console.log('jay'[0].toUpperCase());
+console.log('jay'[0].toUpperCase() + 'jay'[1] + 'jay'[2]);
+
+//Challenge uppercase the'm' in Home and put the string
+// back together using string concatenation (concatenate= link things together in a chain or series)
+
+console.log('Home'[2].toUpperCase());
+console.log('Home'[0]+'Home'[1]+'Home'[2].toUpperCase()+'Home'[3]); //output= HoMe
+
+console.log(typeof("Amber")); //output= string
+
+
+
+
+//Numbers
+
+console.log(typeof(2));
+
+console.log(2+7); // ADD
+console.log(8-5); // SUBTRACT
+console.log(12/3); // DIVIDE
+console.log(8*5); // MULTIPLY
+console.log(8%5); // MODULUS
+
+
+//Booleans
+
+console.log(true);
+
+/*
+	Conditional Operators
+
+	<	: less than
+	>	: greater than
+	<=	: less than or equal to
+	>=	: greater than or equal to
+	==	: equal to
+	===	: strictly equal to
+	!	: not
+	!=	: not equal to
+
+
+*/
+
+console.log(4==5);
+console.log("Test".length > "ego".length);
+console.log(1 == '1');
+console.log
+
+console.log('\n');
