fdc351f16a337bc1a2b5a329600217dbcacd8523
Jared Burris <jaredburris97@gmail.com>
2017-08-16 14:10:26 -0400

    Added variables.js lesson

---
+++ 0.02_variables.js
@@ -0,0 +1,82 @@
+// Variables
+
+var name;
+
+name = "Madyson";
+
+console.log(typeof(name));
+console.log(name);
+
+var lastName = "Ward";
+
+console.log(name + lastName);
+console.log(name.length);
+
+// Challenge create 3 variables 'firstName' and 'lastName' and concat them
+// together in a third variable called 'fullName' and print fullName to the console
+
+var firstName = 'Jared';
+ lastName = 'Burris';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+console.log('\n');
+
+
+//BRONZE CHALLENGE
+
+var username = 'jaredb';
+var password = 'jb111';
+var passcode = 141356;
+var authorize = true;
+
+console.log('Username: '+username+'\n'+'Password: '+password+'\n'+'Passcode: '+passcode+'\n'+'Is this account authorized? '+authorize);
+
+
+
+console.log('\n');
+
+
+//SILVER CHALLENGE
+
+var myNumber = 0;
+console.log(myNumber);
+myNumber = myNumber+10;
+console.log(myNumber);
+myNumber = myNumber-1;
+console.log(myNumber);
+myNumber = myNumber*9;
+console.log(myNumber);
+myNumber = myNumber/7;
+console.log(myNumber);
+
+console.log('\n');
+
+// GOLD CHALLENGE
+
+var currentAge = 20;
+var maxAge = 80;
+var dailyWater = 6;
+var lifeWater = 0;
+
+lifeWater = (maxAge - currentAge)*365*6;
+
+console.log('I will need '+ lifeWater + ' bottles of water to last me until the ripe old age of ' + maxAge + '.');
+
+
+
+
+
+
+
+
+
+// Variable data types
+
+
+// Variable scopes
+
+
+
+
+//
